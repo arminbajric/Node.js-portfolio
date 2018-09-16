fde4beaf1bf8a47567337aa521922b82a58ce976
Armin Bajric <bajric.armin.3t1@gmail.com>
2018-09-16 17:33:44 +0200

    Portfolio

--- portfolio.js
+++ portfolio.js
@@ -58,4 +58,19 @@ else
 				window.document.getElementById("potvrda").value="Potvrdi ";
 		}
 	}
+    function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('footer').innerHTML =
+    h + ":" + m + ":" + s+" By Armin Bajrić";
+    var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
 	
